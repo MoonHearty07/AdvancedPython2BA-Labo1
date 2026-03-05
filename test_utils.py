f6f37a7c801c77ca171bb9bfe1dafcf336f6f71d
Benzha Ismaïl <ibenzha4014@gmail.com>
2026-03-05 14:40:53 +0100

    Add Action Configuration

--- test_utils.py
+++ test_utils.py
@@ -4,11 +4,9 @@ from scipy.integrate import quad
 
 def test_fact():
     assert utils.fact(3) == 6
-    assert utils.fact(4) == 24
 
 def test_roots():
     assert utils.roots(1,0,-1) == (-1,1)
-    assert utils.roots(1,0,-4) == (-2,2)
 
 def test_integrate():
     
